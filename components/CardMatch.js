1fa794ea6d877487882d79706bdb26cb560cfde8
Aman Azad <aman.s.azad@gmail.com>
2018-04-04 12:46:57 -0700

    Minor bug fixes: user cannot click more cards during check, user cannot click same card twice for match. Implementing AI player.

--- components/CardMatch.js
+++ components/CardMatch.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import concat from 'lodash/concat'
 
-const cards = [ "diamondA", "clubA", "heartA", "spadeA",
-                "diamond2", "club2", "heart2", "spade2",
-                "diamond3", "club3", "heart3", "spade3",
-                "diamond4", "club4", "heart4", "spade4",
-                "diamond5", "club5", "heart5", "spade5",
-                "diamond6", "club6", "heart6", "spade6",
-                "diamond7", "club7", "heart7", "spade7",
-                "diamond8", "club8", "heart8", "spade8",
-                "diamond9", "club9", "heart9", "spade9",
-                "diamond10", "club10", "heart10", "spade10",
-                "diamondJ", "clubJ", "heartJ", "spadeJ",
-                "diamondQ", "clubQ", "heartQ", "spadeQ",
-                "diamondK", "clubK", "heartK", "spadeK" ];
+const cards = [ "Diamond A", "Club A", "Heart A", "Spade A",
+                "Diamond 2", "Club 2", "Heart 2", "Spade 2",
+                "Diamond 3", "Club 3", "Heart 3", "Spade 3",
+                "Diamond 4", "Club 4", "Heart 4", "Spade 4",
+                "Diamond 5", "Club 5", "Heart 5", "Spade 5",
+                "Diamond 6", "Club 6", "Heart 6", "Spade 6",
+                "Diamond 7", "Club 7", "Heart 7", "Spade 7",
+                "Diamond 8", "Club 8", "Heart 8", "Spade 8",
+                "Diamond 9", "Club 9", "Heart 9", "Spade 9",
+                "Diamond 10", "Club 10", "Heart 10", "Spade 10",
+                "Diamond J", "Club J", "Heart J", "Spade J",
+                "Diamond Q", "Club Q", "Heart  Q", "Spade Q",
+                "Diamond K", "Club K", "Heart K", "Spade K" ];
+
+
 
 const cardsToMatch = 2;
 
@@ -23,10 +25,16 @@ class CardMatch extends React.Component {
     //Define function bindings
     this.shuffleCards = this.shuffleCards.bind(this);
     this.pickCard = this.pickCard.bind(this);
+    this.toggleAiMode = this.toggleAiMode.bind(this);
+    this.resetGame = this.resetGame.bind(this);
+    this.ignoreUserClicks = false;
     this.state = {
       currentCards: [],
       selectedCards: [],
-      matchedCards: []
+      matchedCards: [],
+      compPlayerMemory: [],
+      compMatchedCards: [],
+      ai: false
     };
   }
   componentDidMount() {
@@ -54,36 +62,44 @@ class CardMatch extends React.Component {
       currentCards: shuffledObjects,
     }));
   }
+  //TODO Extend this function to handle computer player
   pickCard(cardIndex) {
-    if (!this.state.matchedCards.includes(cardIndex)) {
-      //Define temp variables to check if click is valid
-      let curSelectedCards = concat(this.state.selectedCards, cardIndex);
-      let tempCurrentCards = this.state.currentCards;
-      //Set clicked card temporarily to selected, select most recently added card
-      tempCurrentCards[curSelectedCards[curSelectedCards.length-1]].position = "selected";
-      if (curSelectedCards.length == cardsToMatch) {
-        //Set state to show user card has been clicked
-        this.setState({
-          currentCards: tempCurrentCards
-        })
-        //Check for match, update accordingly
-        setTimeout(() => {
-          //isMatch returns the corrected tempCurrentCards if no match found
-          tempCurrentCards = this.isMatch(curSelectedCards, tempCurrentCards);
-          curSelectedCards = [];
+    if (this.ignoreUserClicks !== true) {
+      if (!this.state.matchedCards.includes(cardIndex)) {
+        //Define temp variables to check if click is valid
+        let curSelectedCards = concat(this.state.selectedCards, cardIndex);
+        let tempCurrentCards = this.state.currentCards;
+        //Set clicked card temporarily to selected, select most recently added card
+        tempCurrentCards[curSelectedCards[curSelectedCards.length-1]].position = "selected";
+        if (curSelectedCards.length == cardsToMatch) {
+          //Check that selected cards are different
+          if (curSelectedCards[0] !== curSelectedCards[1]) {
+            //Set state to show user card has been clicked
+            this.setState({
+              currentCards: tempCurrentCards
+            })
+            this.ignoreUserClicks = true;
+            //Check for match, update accordingly
+            setTimeout(() => {
+              //isMatch returns the corrected tempCurrentCards if no match found
+              tempCurrentCards = this.isMatch(curSelectedCards, tempCurrentCards);
+              curSelectedCards = [];
+              this.setState({
+                selectedCards: curSelectedCards,
+                currentCards: tempCurrentCards
+              });
+              this.ignoreUserClicks = false;
+            }, 750);
+          }
+        }
+        else {
+          //First of selected cards, show card face
+          tempCurrentCards[curSelectedCards[0]].position = "selected";
           this.setState({
             selectedCards: curSelectedCards,
             currentCards: tempCurrentCards
-          });
-        }, 750);
-      }
-      else {
-        //First of selected cards, show card face
-        tempCurrentCards[curSelectedCards[0]].position = "selected";
-        this.setState({
-          selectedCards: curSelectedCards,
-          currentCards: tempCurrentCards
-        })
+          })
+        }
       }
     }
   }
@@ -114,22 +130,30 @@ class CardMatch extends React.Component {
   resetGame() {
 
   }
+  toggleAiMode() {
+    this.setState(prevState => ({
+      ai: !prevState.ai
+    }));
+  }
   render() {
     let cardIndex = 0;
     let clickEvent = this.pickCard;
+    let toggleEvent = this.toggleAiMode;
+    let resetEvent = this.resetGame;
     return (
       <div className="card-match-app">
-        <Stats matchedCards={this.state.matchedCards} cards={this.state.currentCards}/>
+        <Stats matchedCards={this.state.matchedCards} cards={this.state.currentCards} aimode={this.state.ai} aiMatchedCards={this.state.compMatchedCards}/>
         <div className="cards">
           {this.state.currentCards.map((card) => {
             return <Card key={cardIndex}
                          index={cardIndex++}
                          type={card.type}
                          position={card.position}
-                         clickEvent={clickEvent}/>
+                         clickEvent={clickEvent}
+                         ignoreUserClicks={this.state.ignoreUserClicks} />
           })}
         </div>
-        <Controls resetFunction={this.resetMatch} />
+        <Controls resetEvent={resetEvent} toggleEvent={toggleEvent} aimode={this.state.ai} />
       </div>
     )
   }
@@ -142,7 +166,7 @@ class Card extends React.Component {
   }
   processClick() {
     //Pass in cardIndex to parent click function
-    if (this.props.clickEvent !== null) {
+    if (this.props.clickEvent !== null && this.props.ignoreUserClicks !== true) {
       this.props.clickEvent(this.props.index);
     }
   }
@@ -150,11 +174,10 @@ class Card extends React.Component {
     return (
       <div data-index={this.props.index}
            data-cardtype={this.props.type}
-           onClick={this.clickMe}
-           className={"card card--"+this.props.type}
-           onClick={this.processClick}>
+           onClick={this.processClick}
+           className={"card card--"+this.props.type}>
         <div className={"card--"+this.props.position}>
-          <p>{this.props.position=="unselected" ? "card_back" : this.props.type}</p>
+          <p>{this.props.position=="unselected" ? "CARD" : this.props.type}</p>
           <div className={this.props.position=="unselected" ? "card_back" : "card_front"}></div>
         </div>
       </div>
@@ -170,6 +193,7 @@ class Stats extends React.Component {
     return (
       <div className="stats">
         <h3>Total Matched Pairs: {this.props.matchedCards.length==0 ? 0 : this.props.matchedCards.length/2}</h3>
+        <h3>AI Matched Pairs: {this.props.aimode ? this.props.aiMatchedCards.length/2 : "AI Turned Off..."}</h3>
         <h3>Matched Pairs: </h3>
         <div className="cards matched">
           {this.props.matchedCards.length==0 ? "None" :
@@ -189,14 +213,21 @@ class Stats extends React.Component {
 class Controls extends React.Component {
   constructor(props) {
     super();
+    this.processClick = this.processClick.bind(this);
+  }
+  processClick() {
+    this.props.toggleEvent();
   }
   render() {
     return (
       <div className="controls">
+        <div className="toggleai"
+             onClick={this.processClick}>
+          <button>{this.props.aimode ? "Turn Off AI" : "Turn On AI"}</button>
+        </div>
       </div>
     )
   }
 }
-
 
 export default CardMatch
